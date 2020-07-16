58f5d5ccb825141645d73559ca18159d15821b20
neosangsang <sangsang73@gmail.com>
2020-07-16 09:26:05 +0900

    first

--- src/main/java/rental/PolicyHandler.java
+++ src/main/java/rental/PolicyHandler.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyHandler{
+    @Autowired
+    AssignRepository assignRepository;
+
     @StreamListener(KafkaProcessor.INPUT)
     public void onStringEventListener(@Payload String eventString){
 
@@ -19,8 +22,14 @@ public class PolicyHandler{
     public void wheneverScheduleFixed_Assign(@Payload ScheduleFixed scheduleFixed){
 
         if(scheduleFixed.isMe()){
+            Assign assign = new Assign();
+            assign.setOrderId(scheduleFixed.getId());
+            assign.setUserId("A0001");
+            assign.setCheckDate(scheduleFixed.getCheckDate());
+            assign.setStatus(assign.getUserId() + " : schedule assigned!");
+            assignRepository.save(assign);
             System.out.println("##### listener Assign : " + scheduleFixed.toJson());
         }
     }
-
 }
+
